746c39f56e9e328043ec0c18ba514a458761e43d
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-09 23:54:00 -0300

    Studies about binding

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+  
+  hotelName = 'Hilton Hotel'
+
+  numberOfRooms = 10;
+
+}
